01ea23c171583920388e9a4ca27c0c605044f92a
ProjectRobal <patryk1-70@wp.pl>
2024-09-12 20:18:33 +0200

    I am adding moving logo

--- main/src/oledModes.cpp
+++ main/src/oledModes.cpp
@@ -3,7 +3,8 @@
 namespace oled_modes
 {
 
-static uint8_t step=0;
+static uint8_t step = 0;
+static bool moveup = true;
 
 // returns page id to switch to ( returns 0 to not switch )
 
@@ -14,11 +15,23 @@ uint8_t main_page(oled::OLED& screen)
     
     screen.drawBitmap(step*16,0,64,32,sprites::konar_logo_64x32);
 
-    step = step + 1;
-
-    if( step == 8 )
+    if(moveup)
     {
-        step = 0;
+        ++step;
+
+        if( step == 8 )
+        {
+            moveup = false;
+        }
+    }
+    else
+    {
+        --step;
+
+        if( step == 0 )
+        {
+            moveup = true;
+        }
     }
 
     // if(gpio_get_level(MODE_BUTTON)!=mode_button_state)
