999eefb1fb2c346929068a3a9d827bc55168268c
ProjectRobal <patryk1-70@wp.pl>
2024-09-12 20:17:04 +0200

    I am adding moving logo

--- main/include/oledModes.hpp
+++ main/include/oledModes.hpp
@@ -16,20 +16,6 @@ namespace oled_modes
 // returns page id to switch to ( returns 0 to not switch )
 
 // show main page ( KoNaR logo )
-uint8_t main_page(oled::OLED& screen)
-{
-
-    screen.drawBitmap(0,0,64,32,sprites::konar_logo_64x32);
-
-    // if(gpio_get_level(MODE_BUTTON)!=mode_button_state)
-    // {
-    //     if(gpio_get_level(MODE_BUTTON))
-    //     {
-    //         return 2;
-    //     }
-    // }
-
-    return 0;
-}
+uint8_t main_page(oled::OLED& screen);
 
 } // namespace oled_modes
