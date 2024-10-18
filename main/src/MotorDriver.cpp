52a4d6ec660320073a19825ac160bcb2889e02bd
ProjectRobal <patryk1-70@wp.pl>
2024-10-18 19:25:05 +0200

    Motors are still repleaced

--- main/src/MotorDriver.cpp
+++ main/src/MotorDriver.cpp
@@ -239,8 +239,8 @@ void MotorDriver::loop()
         float target_speed_left = this->target_speed + D_WHEELS*angular_speed;
         float target_speed_right = this->target_speed - D_WHEELS*angular_speed;
 
-        float dSpeedLeft = abs(target_speed_left) - abs(readings.motorSpeed[0]); 
-        float dSpeedRight = abs(target_speed_right) - abs(readings.motorSpeed[1]); 
+        float dSpeedLeft = abs(target_speed_left) - abs(readings.motorSpeed[1]); 
+        float dSpeedRight = abs(target_speed_right) - abs(readings.motorSpeed[0]); 
 
         int32_t PowerLeft = this->motorLeft.step(dSpeedLeft)*MAX_ENGINE_POWER;
         int32_t PowerRight = this->motorRight.step(dSpeedRight)*MAX_ENGINE_POWER;
