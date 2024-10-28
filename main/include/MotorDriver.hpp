8b7fc87a9a4cdef021d066b3686915b819632421
ProjectRobal <patryk1-70@wp.pl>
2024-10-28 22:15:55 +0100

    That regulator needs some thoughts

--- main/include/MotorDriver.hpp
+++ main/include/MotorDriver.hpp
@@ -61,8 +61,8 @@ class MotorDriver
     motorA(1.f,0.f,0.f),
     motorLeft(1.f,0.f,0.f),
     motorRight(1.f,0.f,0.f),
-    regLeft(0.00036/8.f,0.00036),
-    regRight(0.00036/8.f,0.00036),
+    regLeft(0.00036/8.f,0.00001),
+    regRight(0.00036/8.f,0.00001),
     automaticMode(true)
     {
         this->motorA.setMin(-1.f);
